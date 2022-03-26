8696dca27d96665db0b85822bd1c2a4801da6bb4
dahhnym <danimkim.dev@gmail.com>
2022-03-26 23:53:06 +0900

    🍱 아이콘 svg 추가

--- src/components/assets/index.js
+++ src/components/assets/index.js
@@ -2,5 +2,6 @@ import { ReactComponent as PersonIcon} from './PersonIcon.svg'
 import { ReactComponent as StarIcon} from './StarIcon.svg'
 import { ReactComponent as SearchIcon} from './SearchIcon.svg'
 import { ReactComponent as HomeIcon } from './HomeIcon.svg'
+import { ReactComponent as ArrowLeftIcon } from './ArrowLeftIcon.svg'
 
-export { PersonIcon, StarIcon, SearchIcon, HomeIcon};
+export { PersonIcon, StarIcon, SearchIcon, HomeIcon, ArrowLeftIcon};
