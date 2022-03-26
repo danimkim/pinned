8cb021d0d73fb7e3e3997aecffd97d5f243acb58
dahhnym <danimkim.dev@gmail.com>
2022-03-26 23:54:42 +0900

    🚧 기본 레이아웃, 화면 구현 중

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,49 @@
 import styled from 'styled-components';
 import { SearchIcon } from '../components/assets';
+import Button from '../elements/Button'
 
 const Home = () => {
-    return <div>
-        <p>안녕하세요 Tina님</p>
-        <p>오늘의 마음의 드는 장소는 어디인가요?</p>
+    return (
+    <div>
+        <Greeting>안녕하세요 Tina님</Greeting>
+        <SubGreeting>오늘의 마음의 드는 장소는 어디인가요?</SubGreeting>
         <InputBox>
             <SearchIcon />
             <Input type="text" placeholder="Search..."/>
         </InputBox>
+        <Button>저장한 장소</Button>
+        <Button>방문한 장소</Button>
+        <Button>지역별</Button>
+        <Button>카테고리별</Button>
+        <Button>내 취저 장소</Button>
     </div>
+    )
 }
 
 export default Home;
 
 const InputBox = styled.div`
-    border: solid 1px red;
+    border: solid 1px #A2ADB1;
     height: 3.125rem;
     border-radius: 1.5rem;
     padding-left: 1rem;
+    background-color: #fff;
 `
 
 const Input = styled.input`
     border: solid 1px blue;
     height: inherit;
     margin-left: 0.8rem;
+    width: calc(100% - 2rem);
+`
+
+const Greeting = styled.p`
+    font-size: 1.5rem;
+    padding-bottom: 0.5rem;
+`
+
+const SubGreeting = styled.p`
+    font-size: 1.875rem;
+    margin-bottom: 2rem;
 `
 
---
+++ src/pages/Search.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { SearchIcon } from '../components/assets';
+
+const Search = () => {
+    return (
+        <div>
+            <InputBox>
+            <SearchIcon />
+            <Input type="text" placeholder="Search..."/>
+        </InputBox>
+        </div>
+    )
+}
+
+export default Search;
+
+const InputBox = styled.div`
+    border: solid 1px #A2ADB1;
+    height: 3.125rem;
+    border-radius: 1.5rem;
+    padding-left: 1rem;
+    background-color: #fff;
+`
+
+const Input = styled.input`
+    border: solid 1px blue;
+    height: inherit;
+    margin-left: 0.8rem;
+    width: calc(100% - 2rem);
+`
