a34e59e2b7f7448ed86f5e5411c7d500915f4f8f
dahhnym <danimkim.dev@gmail.com>
2022-03-28 01:20:15 +0900

    🍱 아이콘 svg 추가

--- src/components/assets/index.js
+++ src/components/assets/index.js
@@ -5,5 +5,8 @@ import { ReactComponent as HomeIcon } from './HomeIcon.svg'
 import { ReactComponent as ArrowLeftIcon } from './ArrowLeftIcon.svg'
 import { ReactComponent as DeleteIcon } from './DeleteIcon.svg'
 import { ReactComponent as EditIcon } from './EditIcon.svg'
+import { ReactComponent as LockIcon } from './LockIcon.svg'
+import { ReactComponent as MailIcon } from './MailIcon.svg'
+import { ReactComponent as PersonEmptyIcon } from './PersonEmptyIcon.svg'
 
-export { PersonIcon, StarIcon, SearchIcon, HomeIcon, ArrowLeftIcon, DeleteIcon, EditIcon};
+export { PersonIcon, StarIcon, SearchIcon, HomeIcon, ArrowLeftIcon, DeleteIcon, EditIcon, LockIcon, MailIcon, PersonEmptyIcon};
