e085b532f7c446bd04e65943b00c2049895a0758
dahhnym <danimkim.dev@gmail.com>
2022-03-28 01:23:06 +0900

    ♻️ 라우터에 따른 header, footer 표시

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import styled from 'styled-components';
-import Home from './pages/Home';
-import Splash from './pages/Splash';
-import SignUp from './pages/SignUp';
-import Footer from './components/Footer';
-import Profile from './pages/Profile';
-import Search from './pages/Search';
-import New from './pages/New';
-import Header from './components/Header';
-import List from './pages/List';
-import View from './pages/View';
+import "./App.css";
+import { Routes, Route, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import Home from "./pages/Home";
+import Splash from "./pages/Splash";
+import SignUp from "./pages/SignUp";
+import Footer from "./components/Footer";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+import New from "./pages/New";
+import Header from "./components/Header";
+import List from "./pages/List";
+import View from "./pages/View";
+import Login from "./pages/Login";
 
 function App() {
+  const location = useLocation();
+    const noHeaderPathnames = ["/signup", "/", "/home"]
+    const noFooterPathnames = ["/signup", "/"]
+
   return (
     <div className="App">
-    <Header />
-    <ContentWrapper>
+      {!noHeaderPathnames.includes(location.pathname) && <Header />}
+      <ContentWrapper>
         <Routes>
-            <Route path="/" element={<Splash />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/new" element={<New />} />
-            <Route path="/view" element={<View />} />
-            <Route path="/list" element={<List />} />
+          <Route path="/" element={<Splash />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/view" element={<View />} />
+          <Route path="/list" element={<List />} />
         </Routes>
-    </ContentWrapper>
-    <Footer />
+      </ContentWrapper>
+      {!noFooterPathnames.includes(location.pathname) && <Footer />}
     </div>
   );
 }
@@ -36,5 +42,5 @@ function App() {
 export default App;
 
 const ContentWrapper = styled.div`
-    padding: 1.5rem;
-`
+  padding: 1.5rem;
+`;
