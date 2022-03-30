19921b5bccea94b56ceb20b64c50edf9dc9d39a4
dahhnym <danimkim.dev@gmail.com>
2022-03-30 15:20:06 +0900

    ✨ 프로필 페이지 추가

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,59 @@
+import {PersonIcon} from "../components/assets"
+import styled from 'styled-components'
+import SubmitBtn from "../elements/SubmitBtn";
+
 const Profile = () => {
     return (
-        <h1>프로필</h1>
+        <div>
+            <Wrapper>
+                <ProfilePic>
+                    <PersonIcon />
+                    <AddBtn>+</AddBtn>
+                </ProfilePic>            
+                <div>
+                    <Name>Tina Kim</Name>
+                </div>
+            </Wrapper>
+            <SubmitBtn>회원정보 수정</SubmitBtn>
+        </div>
+
     )
 }
 
 export default Profile;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 60vh;
+    align-items: center;
+`
+
+const ProfilePic = styled.div`
+    background-color: #fff;
+    padding: 2rem;
+    display: inline-block;
+    border-radius: 2rem;
+    position: relative;
+    margin-bottom: 1.2rem;
+`
+
+const AddBtn = styled.button`
+    background-color: #04202F;
+    color: #fff;
+    width: 1.8rem;
+    height: 1.8rem;
+    border-radius: 0.8rem;
+    position: absolute;
+    bottom: -5px;
+    right: -5px;
+    &:hover {
+        cursor: pointer;
+    }
+`
+
+const Name = styled.p`
+    text-align: center;
+    font-weight: 700;
+`
