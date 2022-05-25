1bf56a39582c8fa829572ba57dfa3654a925759a
Danim Kim <kdn725@gmail.com>
2022-05-25 12:34:00 +0900

    ♻️ modified code due to react dependency upgrades

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
 );
