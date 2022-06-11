660f1a96626f2a24c281e8d421156d24f31f9cd1
Danim Kim <kdn725@gmail.com>
2022-06-12 00:47:58 +0900

    ♻️ add event listener for firebase initialization

--- src/App.tsx
+++ src/App.tsx
@@ -16,47 +16,57 @@ import { auth } from './fbase';
 import Signup from './pages/Signup';
 import EditProfile from './components/EditProfile';
 import NotFound from './pages/NotFound';
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const location = useLocation();
   const noHeaderPathnames = ['/auth', '/', '/home'];
   const noFooterPathnames = ['/auth', '/', '/login'];
 
-  const contentInner = useRef<HTMLDivElement>(null);
-  const [contentInnerHeight, setContentInnerHeight] = useState<number>(
-    window.innerHeight,
-  );
-  const [headerHeight, setHeaderHeight] = useState<number>(0);
-  const [footerHeight, setFooterHeight] = useState<number>(0);
-
-  const heightChange = window.innerHeight - headerHeight - footerHeight;
-  console.log('heightChange', heightChange);
+  // firebase가 초기화 될때, 계정이 생성되었을때, 로그인, 로그아웃 됐을때 currentUser의 상태를 알려준다.
+  useEffect(() => {
+    // 일종의 event listener 역할을 한다.
+    // 계정 생성 버튼을 눌렀거나, 로그인을 했거나, 이미 로그인 되어 있는 상태에서 firebase가 초기화하는 것을 끝냈을때
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
 
   return (
     <>
-      {!noHeaderPathnames.includes(location.pathname) && auth.currentUser && (
-        <Header setHeaderHeight={setHeaderHeight} />
-      )}
-      <ContentWrapper ref={contentInner}>
-        <Routes>
-          <Route path="/" element={<Splash />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/profile" element={<Profile />}>
-            <Route path="edit" element={<EditProfile />} />
-          </Route>
-          <Route path="/search" element={<Search />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/view" element={<View />} />
-          <Route path="/list" element={<List />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </ContentWrapper>
-      {!noFooterPathnames.includes(location.pathname) && auth.currentUser && (
-        <Footer setFooterHeight={setFooterHeight} />
+      {init ? (
+        <Container>
+          {!noHeaderPathnames.includes(location.pathname) &&
+            auth.currentUser && <Header />}
+          <ContentWrapper>
+            <Routes>
+              <Route path="/" element={<Splash />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/profile" element={<Profile />}>
+                <Route path="edit" element={<EditProfile />} />
+              </Route>
+              <Route path="/search" element={<Search />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/view" element={<View />} />
+              <Route path="/list" element={<List />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </ContentWrapper>
+          {!noFooterPathnames.includes(location.pathname) &&
+            auth.currentUser && <Footer />}
+        </Container>
+      ) : (
+        'Initializing...'
       )}
     </>
   );
@@ -68,5 +78,9 @@ const ContentWrapper = styled.div`
   padding: 2.5rem;
   max-width: 640px;
   margin: 0 auto;
+`;
+
+const Container = styled.div`
   border: 1px solid red;
+  height: 100dvh;
 `;
